e9c43a1385a6fbdb1af79d118cab9e90a18df46a
faening <cmorais58@gmail.com>
2023-07-28 18:11:50 -0300

    feat: initial commit

---
+++ src/test/java/br/com/faening/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
